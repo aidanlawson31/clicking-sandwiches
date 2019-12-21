ea642ec51c55618940ba653dc19c3f2b9e01da89
AidanL <aidanlawson31@gmail.com>
2019-12-21 14:59:47 +1100

    admin true and false boolean

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
   protected
   
   def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:admin, :email, :password)}
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:admin, :email, :password)}
 
-      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:admin, :email, :password, :current_password)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:admin, :email, :password, :current_password)}
   end
 end
