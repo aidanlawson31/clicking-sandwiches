38fbae805abd4a0d5f377efaa068bdd5711dbdca
AidanL <aidanlawson31@gmail.com>
2020-02-12 16:52:07 +1100

    some forms

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -25,7 +25,7 @@ class LocationsController < ApplicationController
 
   def update
     @location = Location.find(params[:id])
-    @location.menus.create(menu_id: menu.id)
+    @location.location_menus.create(menu_id: menu.id)
 
     if @location.update(location_params)
       redirect_to location_path(@location), notice: "Location updated successfully"
